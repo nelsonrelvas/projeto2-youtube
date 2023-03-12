9e08d7b0d2e66f9b455341ca6994f0195a3a18c3
fazaido <nelson.srelvas@gmail.com>
2023-03-12 18:11:50 -0300

    header - parte 2

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,26 @@
-import { Container, LogoContainer, ButtonContainer, ButtonIcon } from "./stype";
+import {
+    Container,
+    LogoContainer,
+    ButtonContainer,
+    ButtonIcon,
+    SearchContainer,
+    SearchInputContainer,
+    SearchInput,
+    SearchButton,
+    HeaderButton
+} from "./stype";
 import Hamburger from '../../asserts/hamburger.png';
 import Logo from '../../asserts/YouTube-Logo.png'
+import SearchIcon from '../../asserts/search.png'
+import MicIcon from '../../asserts/microfone-gravador.png'
+import VideoIcon from '../../asserts/video.png'
+import NotificationIcon from '../../asserts/sino.png'
 
 function Header() {
     return (
         <Container>
             <LogoContainer>
-                <ButtonContainer>
+                <ButtonContainer margin='0 10px 0 0'>
                     <ButtonIcon alt="Hamburger" src={Hamburger} />
                 </ButtonContainer>
                 <img
@@ -15,6 +29,30 @@ function Header() {
                     src={Logo}
                 />
             </LogoContainer>
+
+            <SearchContainer>
+                <SearchInputContainer>
+                    <SearchInput placeholder="Pesquisar" />
+                </SearchInputContainer>
+                <SearchButton>
+                    <ButtonIcon alt="Buscar" src={SearchIcon} />
+                </SearchButton>
+                <ButtonContainer margin='0 0 0 10px'>
+                    <ButtonIcon alt="Phone" src={MicIcon} />
+                </ButtonContainer>
+            </SearchContainer>
+
+            <HeaderButton>
+                <ButtonContainer margin='0 0 0 10px'>
+                    <ButtonIcon alt="Video" src={VideoIcon} />
+                </ButtonContainer>
+                <ButtonContainer margin='0 0 0 10px'>
+                    <ButtonIcon alt="Notificação" src={NotificationIcon} />
+                </ButtonContainer>
+                <ButtonContainer margin='0 0 0 10px'>
+                    P
+                </ButtonContainer>
+            </HeaderButton>
         </Container>
     )
 }
