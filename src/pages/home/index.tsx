b296ef7e0caba3cea4a302e9323d82edfe4088ca
fazaido <nelson.srelvas@gmail.com>
2023-03-15 13:11:34 -0300

    página inicial

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,10 +5,27 @@ function Home() {
 
     return (
         <Container>
-            <VideoComponent urlImage="https://i.ytimg.com/vi/tI55Zu9uZEM/maxresdefault.jpg" 
-                            title="Marília Mendonça - Leão - Decretos Reais" 
-                            titleCard="Marília Mendonça" 
-                            titleCardCont="109 mi de visualizações - há 2 meses" />
+        <VideoComponent urlImage="https://i.ytimg.com/vi/tI55Zu9uZEM/maxresdefault.jpg" 
+                        title="Marília Mendonça - Leão - Decretos Reais" 
+                        titleCard="Marília Mendonça" 
+                        titleCardCont="109 mi de visualizações - há 2 meses" />
+
+        <VideoComponent urlImage="https://i.ytimg.com/vi/tI55Zu9uZEM/maxresdefault.jpg" 
+                        title="Mendonça - Leão - Decretos Reais" 
+                        titleCard="Marília" 
+                        titleCardCont="409 mi de visualizações - há 2 meses" />
+
+
+        <VideoComponent urlImage="https://i.ytimg.com/vi/tI55Zu9uZEM/maxresdefault.jpg" 
+                        title="Leão - Decretos Reais" 
+                        titleCard="Mendonça" 
+                        titleCardCont="309 mi de visualizações - há 2 meses" />
+
+
+        <VideoComponent urlImage="https://i.ytimg.com/vi/tI55Zu9uZEM/maxresdefault.jpg" 
+                        title="Decretos Reais" 
+                        titleCard="João" 
+                        titleCardCont="209 mi de visualizações - há 2 meses" />
         </Container>
     );
 }
